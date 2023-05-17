df922cd8baf55039d7450cef3518d85969c9f60f
Roy Mbaya <roymbaya08@gmail.com>
2023-05-17 16:42:17 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    # Instance method definition
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+# Woof!
+fido.sit()
+# The dog is sitting.
+
+snoopy = Dog()
+snoopy.bark()
+# Woof!
+snoopy.sit()
+# The dog is sitting.
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+from lib.person import Person
+
+person = Person()
+person.talk()
+# Hello World!
+
+person.walk()
+# The person is walking.
